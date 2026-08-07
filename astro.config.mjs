c898f70adb9649f2f4a41515af059a7eeb184268
devcode-os <fdaytalk@gmail.com>
2026-08-08 01:16:40 +0530

    mobile ux fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import mkcert from 'vite-plugin-mkcert';
 export default defineConfig({
   output: 'static',
   site: 'https://your-site.pages.dev',
+  devToolbar: {
+    enabled: false
+  },
   vite: {
     plugins: [mkcert()],
     server: {
